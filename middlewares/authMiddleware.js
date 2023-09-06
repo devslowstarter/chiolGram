14b90e8631f863170c2a9d1cc81ecfe4de0c863d
tuy112 <master_catch@naver.com>
2023-09-06 21:12:47 +0900

    fix:user 수정 및 authMiddleware 수정 + 게시판 일부 수정

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,10 +1,14 @@
 const jwt = require('jsonwebtoken');
 const { Users } = require('../models');
 
+require('dotenv').config();
+
 // 사용자 인증 미들웨어
 module.exports = async (req, res, next) => {
-  const { Authorization } = req.cookies;
-  const [authType, authToken] = (Authorization ?? '').split(' ');
+  const { authorization } = req.cookies;
+  // console.log(req.cookies);
+  // console.log(authorization);
+  const [authType, authToken] = (authorization ?? '').split(' ');
 
   if (!authToken || authType !== 'Bearer') {
     res.status(403).send({
@@ -14,17 +18,20 @@ module.exports = async (req, res, next) => {
   }
 
   try {
-    const { userId } = jwt.verify(authToken, process.env.secretKey);
+    // const { userId } = jwt.verify(authToken, process.env.secretKey);
+    // console.clear();
+    console.log('시크릿키:', process.env.CUSTOMIZED_SECRET_KEY);
+    const { userId } = jwt.verify(authToken, process.env.CUSTOMIZED_SECRET_KEY);
     const user = await Users.findOne({ where: { userId } });
     if (!user) {
-      res.clearCookie('Authorization');
+      res.clearCookie('authorization');
       return res.status(401).json({ errorMessage: '토큰 사용자가 존재하지 않습니다.' });
     }
     res.locals.user = user;
     next();
   } catch (err) {
     console.error(err);
-    res.clearCookie('Authorization');
+    res.clearCookie('authorization');
     res.status(403).send({
       errorMessage: '전달된 쿠키에서 오류가 발생하였습니다.',
     });
