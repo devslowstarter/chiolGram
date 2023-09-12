f692fbbb1289955fd7f7af7f3f142351aa6e2c6f
tuy112 <master_catch@naver.com>
2023-09-12 21:05:05 +0900

    fix:userRepository 수정

--- repository/userRepository.js
+++ repository/userRepository.js
@@ -1,11 +1,26 @@
 const { Users } = require('../models');
 
 class UserRepository {
-  // 회원가입
-  signupUser = async (signupData) => await Users.create(signupData);
+  // loginId로 회원 조회
+  findUser = async (loginId) => {
+    return await Users.findOne({ where: { loginId } });
+  };
+  // 회원가입 API
+  createUser = async (loginId, hashPassword, nickname) => {
+    await Users.create({ loginId, password: hashPassword, nickname });
+  };
+  //회원 정보 수정 API
+  updateUser = async (userId, hashPassword, nickname) => {
+    const updateValues = {};
+    if (hashPassword) updateValues.password = hashPassword;
+    if (nickname) updateValues.nickname = nickname;
 
-  // DB에서 User 찾기
-  findOneUser = async (findOption) => await Users.findOne({ where: findOption });
+    await Users.update(updateValues, { where: { userId } });
+  };
+  //회원 탈퇴 API
+  deleteUser = async (userId) => {
+    await Users.destroy({ where: { UserId: userId } });
+  };
 }
 
 module.exports = UserRepository;
